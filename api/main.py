9f1f42d5f3005225b3d94a27f8ab178fbea04a73
Khalid Rizki Ananta <khalidrizki01@mail.ugm.ac.id>
2023-06-16 17:11:12 +0700

    update process.py

--- api/main.py
+++ api/main.py
@@ -9,28 +9,30 @@ load_dotenv()
 
 app = FastAPI()
 
+CONN = os.getenv("MONGODB_URL")
+DB = os.getenv("DATABASE")
+MENU_COLLECTION = os.getenv("PRODUCT_COLLECTION")
+
 
 @app.post("/predict")
 async def predict(request: Request):
     try:
         user = await request.json()
 
-        menu_df = get_menu_data(os.getenv("MONGODB_URL"), 'recomenu', 'Food')
+        menu_docs, menu_df = get_menu_data(CONN, DB, MENU_COLLECTION)
 
-        user_id, user_df = parse_user_data(user)
+        user_df = parse_user_data(user)
 
-        menu_ids, menu_vecs, user_vecs = preprocess(menu_df, user_df)
+        menu_vecs, user_vecs = preprocess(menu_df, user_df)
 
         model = tf.keras.models.load_model('api/modelv2')
 
         predictions = model.predict([user_vecs, menu_vecs])
 
-        rec = get_recommended_foods(predictions, menu_ids)
+        rec = get_recommended_foods(predictions, menu_docs)
 
-        return {'uid': user_id, 'recommendation': rec}
+        return rec
 
     except Exception as e:
         # Handle the exception
         return {"error", str(e)}
-
-
--- api/process.py
+++ api/process.py
@@ -7,145 +7,115 @@ import tensorflow as tf
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
 
+from bson import ObjectId
+import pymongo
+import pandas as pd
+
 def get_menu_data(conn, database_name, collection_name):
     # Koneksi ke MongoDB
-    client = MongoClient(conn)
+    client = pymongo.MongoClient(conn)
     db = client[database_name]
     collection = db[collection_name]
 
     # Ambil semua item food dari database
     items = collection.find()
 
-    # List untuk menyimpan data
+    result = {}
     data = []
+    
+    # Daftar bahan dan kolom yang sesuai
+    ingredients_dict = {
+        "pepperoni": "pepperoni",
+        "daging": "daging",
+        "ayam": "ayam",
+        "tomat": "tomat",
+        "paprika": "paprika",
+        "saus": "saus",
+        "merica": "merica",
+        "jamur": "jamur",
+        "cheese": "keju",
+        "vegetable": "sayur"
+    }
+    
+    for i, item in enumerate(items):
+        # Ubah _id menjadi string
+        item['_id'] = str(item['_id'])
+        result[i] = item
+        
+        if 'spiceLevel' in item:
+          spiceLevel = item['spiceLevel']
+        else:
+          spiceLevel = 1
 
-    # Loop melalui setiap item food
-    for item in items:
-        # Ambil nilai field-field yang diperlukan
-        food_id = item["_id"]
-        name = item["name"]
-        spice_level = item["spiceLevel"]
-        image_url = item["image"]
-        ingredients = item["ingredient"]
-        description = item["description"]
-        price = item["price"]
-        richDescription = item["richDescription"]
+        ingredient_values = {column: 0 for column in ingredients_dict.values()}  # Set all values to zero initially
+        
+        if 'ingredient' in item:
+            ingredients = item['ingredient']
+            
+            if isinstance(ingredients, str):  # Handle case when ingredient is a string
+                ingredients = [ingredients.lower()]
+            elif isinstance(ingredients, list):  # Handle case when ingredient is an array
+                ingredients = [ingredient.lower() for ingredient in ingredients]
 
-        # Inisialisasi nilai kolom bahan
-        pepperoni = 0
-        beef = 0
-        chicken = 0
-        tomatoes = 0
-        capsicum = 0
-        sauce = 0
-        peppers = 0
-        mushroom = 0
-        cheese = 0
-        vegetable = 0
-
-        # Periksa setiap bahan dan atur nilai kolom bahan yang sesuai
-        if "pepperoni" in ingredients:
-            pepperoni = 1
-        if "beef" in ingredients:
-            beef = 1
-        if "chicken" in ingredients:
-            chicken = 1
-        if "tomatoes" in ingredients:
-            tomatoes = 1
-        if "capsicum" in ingredients:
-            capsicum = 1
-        if "sauce" in ingredients:
-            sauce = 1
-        if "peppers" in ingredients:
-            peppers = 1
-        if "mushroom" in ingredients:
-            mushroom = 1
-        if "cheese" in ingredients:
-            cheese = 1
-        if "vegetable" in ingredients:
-            vegetable = 1
+            # Periksa setiap bahan dan atur nilai kolom yang sesuai
+            ingredient_values = {column: 1 if ingredient in ingredients else 0 for ingredient, column in ingredients_dict.items()}
 
         # Tambahkan data ke list
         data.append({
-            "food_id": food_id,
-            "Name of Pizza": name,
-            "Spice Level": spice_level,
-            "Pepperoni": pepperoni,
-            "Beef": beef,
-            "Chicken": chicken,
-            "Tomatoes": tomatoes,
-            "Capsicum": capsicum,
-            "Sauce": sauce,
-            "Peppers": peppers,
-            "Mushroom": mushroom,
-            "Cheese": cheese,
-            "Vegetable": vegetable,
-            "Image_url": image_url
-        })
+        "Spice Level": spiceLevel,
+        **ingredient_values
+    })
 
     # Tutup koneksi MongoDB
     client.close()
-
+    
     # Buat dataframe dari list
     df = pd.DataFrame(data)
-    return df
-
+    return result, df
 
 def parse_user_data(user):
-    data = []
+  data = []
+      
+  # Daftar bahan dan kolom yang sesuai
+  likeIngredients_dict = {
+      "pepperoni": "pepperoni",
+      "daging": "daging",
+      "ayam": "ayam",
+      "tomat": "tomat",
+      "paprika": "paprika",
+      "saus": "saus",
+      "merica": "merica",
+      "jamur": "jamur",
+      "cheese": "keju",
+      "vegetable": "sayur"
+  }
+      
+  if 'spiceLevel' in user:
+    spiceLevel = user['spiceLevel']
+  else:
+    spiceLevel = 1
 
-    user_id = user['_id']
-    name = user["name"]
-    spice_level = user["spiceLevel"]
-    # image_url = item["imageUrl"]
-    ingredients = user["likedIngredients"]
+  likeIngredient_values = {column: 0 for column in likeIngredients_dict.values()}  # Set all values to zero initially
+  
+  if 'likeIngredient' in user:
+      likeIngredients = user['likeIngredient']
+      
+      if isinstance(likeIngredients, str):  # Handle case when ingredient is a string
+          likeIngredients = [likeIngredients.lower()]
+      elif isinstance(likeIngredients, list):  # Handle case when ingredient is an array
+          likeIngredients = [ingredient.lower() for ingredient in likeIngredients]
 
-    # Inisialisasi nilai kolom bahan
-    pepperoni = 0
-    beef = 0
-    chicken = 0
-    tomatoes = 0
-    capsicum = 0
-    sauce = 0
-    peppers = 0
-    mushroom = 0
-    cheese = 0
+      # Periksa setiap bahan dan atur nilai kolom yang sesuai
+      likeIngredient_values = {column: 1 if likeIngredient in likeIngredients else 0 for likeIngredient, column in likeIngredients_dict.items()}
 
-    # Periksa setiap bahan dan atur nilai kolom bahan yang sesuai
-    if "pepperoni" in ingredients:
-        pepperoni = 1
-    if "beef" in ingredients:
-        beef = 1
-    if "chicken" in ingredients:
-        chicken = 1
-    if "tomatoes" in ingredients:
-        tomatoes = 1
-    if "capsicum" in ingredients:
-        capsicum = 1
-    if "sauce" in ingredients:
-        sauce = 1
-    if "peppers" in ingredients:
-        peppers = 1
-    if "mushroom" in ingredients:
-        mushroom = 1
-    if "cheese" in ingredients:
-        cheese = 1
 
-    # Tambahkan data ke dataframe
-    data.append({
-                "Spice Level": spice_level,
-                "Pepperoni": pepperoni,
-                "Beef": beef,
-                "Chicken": chicken,
-                "Tomatoes": tomatoes,
-                "Capsicum": capsicum,
-                "Sauce": sauce,
-                "Peppers": peppers,
-                "Mushroom": mushroom,
-                "Cheese": cheese
-            })
-    df = pd.DataFrame(data)
-    return user_id, df
+  # Tambahkan data ke list
+  data.append({
+  "Spice Level": spiceLevel,
+  **likeIngredient_values
+  
+  })
+  return pd.DataFrame(data)
 
 def encode_spice_level(menu_df, user_df):
   scaler = MinMaxScaler(feature_range=(0, 1))
@@ -155,16 +125,19 @@ def encode_spice_level(menu_df, user_df):
 def preprocess(menu_df, user_df):
   num_foods = menu_df.shape[0]
   encode_spice_level(menu_df, user_df)
-  menu_ids = menu_df[['food_id', 'Name of Pizza']]
+#   menu_ids = menu_df[['food_id', 'Name of Pizza']]
   # menu_ids.set_index('food_id', inplace=True)
-  menu_vecs = menu_df[['Spice Level','Pepperoni','Beef','Chicken','Tomatoes','Capsicum','Sauce','Peppers','Mushroom','Cheese','Vegetable']].to_numpy(dtype=float)
+  menu_vecs = menu_df[['Spice Level','pepperoni','daging','ayam','tomat','paprika','saus','merica','jamur','keju','sayur']].to_numpy(dtype=float)
   user_vecs = np.tile(user_df.to_numpy(dtype=float), (num_foods, 1))
-  return menu_ids, menu_vecs, user_vecs
+  return menu_vecs, user_vecs
 
-def get_recommended_foods(predictions, menu_ids):
+def get_recommended_foods(predictions, menu):
     sorted_index = np.argsort(-predictions,axis=0).reshape(-1).tolist()
+    top_index = sorted_index[:3]
+    return [menu[idx] for idx in top_index]
     # sorted_predictions   = predictions[sorted_index]
-    sorted_items = menu_ids.iloc[sorted_index, :].reset_index(drop=True)
-    #   print(sorted_items.food_id[0])
-    top_6 = sorted_items.loc[:5, 'food_id'].tolist()
-    return [str(object_id) for object_id in top_6]
+    # sorted_items = menu_ids.iloc[sorted_index, :].reset_index(drop=True)
+    # #   print(sorted_items.food_id[0])
+    # top_4 = sorted_items.loc[:3, 'food_id'].tolist()
+
+    # return [str(object_id) for object_id in top_4]
